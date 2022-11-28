bb9994398d6541c633a80014d4954cde4d4857ac
FlavioCir <scatto2003@gmail.com>
2022-11-28 16:46:59 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Todo } from './models/todo';
+import { getTodos } from './service/todos.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Test1';
+    title = 'Test1';
+    todos!: Todo[]
+    constructor() {
+        getTodos().then(todos => {
+            console.log(todos)
+            this.todos = todos
+            this.completeAll()
+            console.log(this.todos)
+        })
+    }
+
+    completeAll() {
+        this.todos = this.todos.map(todo => {
+            return { ...todo, completed: true }
+        })
+    }
 }
